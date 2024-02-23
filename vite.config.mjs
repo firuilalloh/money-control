eb87e67076b62d3de828c190d53e276a8dbf552a
firuilalloh <callazar1234@gmail.com>
2024-02-23 14:48:44 +0700

    edit vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import {fileURLToPath, URL} from "node:url"
+import {fileURLToPath} from "node:url"
+import path from "node:path";
+
+const filename = fileURLToPath(import.meta.url)
+const pathSegments = path.dirname(filename)
 
 export default defineConfig({
   plugins: [vue()],
@@ -9,7 +13,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        // '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@': path.resolve(pathSegments, './src')
     },
     extensions:
     ['.mjs','.js','.ts','.jsx','.tsx','.json','.vue']
